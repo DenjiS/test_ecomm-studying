63d1568cc6eba674121ee2f1531012209fdec889
BaksCoder <Sychkov.Daniil@yandex.ru>
2022-07-20 01:54:40 +0300

    -added good test model -made article validation with regex

--- ecomm_platform/store/admin.py
+++ ecomm_platform/store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Good
+
 
 # Register your models here.
+@admin.register(Good)
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ('article', 'type', 'name', 'price')
+
--- ecomm_platform/store/models.py
+++ ecomm_platform/store/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
 
 # Create your models here.
+class ArticleField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 3 letters in areas [A-I][J-R][S-Z], 4 nulls, 3 any digits
+        self.validators.append(RegexValidator(regex=r'^[A-I]{1}[J-R]{1}[S-Z]{1}0{4}\d{3}$'))
+
+
+class Good(models.Model):
+    TYPE_CHOICES = [
+        ('Комплектующие ПК', (
+            ('cpu', 'CPU'),
+            ('motherboard', 'материнская плата'),
+            ('gpu', 'GPU'),
+            ('ram', 'RAM'),
+        )
+         ),
+        ('Периферийные устройства', (
+            ('mouse', 'мышь'),
+            ('keyboard', 'клавиатура'),
+        )
+         ),
+        ('Остальное', (
+            ('ties', 'кабельные стяжки'),
+        )
+         ),
+    ]
+    article_num = ArticleField(max_length=10)
+    name = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    type = models.CharField(max_length=30, choices=TYPE_CHOICES)
