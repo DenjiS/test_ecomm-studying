3b90ad342e2c762a470d67eef75b119df4c82493
Daniil <sychkov.daniil@yandex.ru>
2022-09-25 01:27:58 +0300

    editable categories, images: (albums, paths), tags, price upd, other

--- ecomm_platform/store/models.py
+++ ecomm_platform/store/models.py
@@ -1,35 +1,49 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator
 
 
-# Create your models here.
-class ArticleField(models.CharField):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # 3 letters in areas [A-I][J-R][S-Z], 4 nulls, 3 any digits
-        self.validators.append(RegexValidator(regex=r'^[A-I]{1}[J-R]{1}[S-Z]{1}0{4}\d{3}$'))
+def get_upload_path(instance, filename):
+    if hasattr(instance, 'cat_model'):
+        return f'images/cat/{instance.album.cat_model.name}/{filename}'
+    elif hasattr(instance, 'prod_model'):
+        return f'images/cat/{instance.album.prod_model.name}/{filename}'
 
 
-class Good(models.Model):
-    TYPE_CHOICES = [
-        ('Комплектующие ПК', (
-            ('cpu', 'CPU'),
-            ('motherboard', 'материнская плата'),
-            ('gpu', 'GPU'),
-            ('ram', 'RAM'),
-        )
-         ),
-        ('Периферийные устройства', (
-            ('mouse', 'мышь'),
-            ('keyboard', 'клавиатура'),
-        )
-         ),
-        ('Остальное', (
-            ('ties', 'кабельные стяжки'),
-        )
-         ),
-    ]
-    article_num = ArticleField(max_length=10)
-    name = models.CharField(max_length=50)
-    price = models.PositiveIntegerField()
-    type = models.CharField(max_length=30, choices=TYPE_CHOICES)
+class ImageAlbum(models.Model):
+    def default(self):
+        return self.images.filter(default=True).first()
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to=get_upload_path)
+    default = models.BooleanField(default=False)
+    album = models.ForeignKey(ImageAlbum, on_delete=models.CASCADE, related_name='images')
+
+
+class Tag(models.Model):
+    word = models.CharField(max_length=63)
+    url = models.URLField(default=None)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    album = models.OneToOneField(ImageAlbum, on_delete=models.CASCADE, related_name='model_cat')
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+class Product(models.Model):
+    article_name = models.CharField(
+        max_length=10,
+        validators=[RegexValidator(regex=r'^[A-I]{1}[J-R]{1}[S-Z]{1}0{4}\d{3}$'), ]
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(limit_value=0.99), ]
+    )
+    name = models.CharField(max_length=255)
+    album = models.OneToOneField(ImageAlbum, on_delete=models.CASCADE, related_name='prod_model')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='products')
