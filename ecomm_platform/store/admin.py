9cb61bc6186433a7d619a94ea6c444e2fc0e870e
Daniil <sychkov.daniil@yandex.ru>
2022-09-25 01:27:48 +0300

    editable categories, images: (albums, paths), tags, price upd, other

--- ecomm_platform/store/admin.py
+++ ecomm_platform/store/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Good
+from .models import Product
 
 
 # Register your models here.
-@admin.register(Good)
-class GoodAdmin(admin.ModelAdmin):
-    list_display = ('article', 'type', 'name', 'price')
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('article_name', 'price', 'name')
 
