7fa0a6144b5381dd5c4298951dfd70d761086911
Daniil <sychkov.daniil@yandex.ru>
2022-10-02 02:46:38 +0300

    catalog page pre-draft

--- ecomm_platform/store/views.py
+++ ecomm_platform/store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Product
+
+
+def catalog(request):
+    product_model = Product.objects.all()
+    template = loader.get_template('store/index.html')
+    context = {
+        'product_model': product_model
+    }
+    return HttpResponse(template.render(context, request))
