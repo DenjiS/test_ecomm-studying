c59f66d77aed3e06f9f5b1e832cd73d104c91a5b
Daniel <109020950+DenjiS@users.noreply.github.com>
2022-10-10 21:54:12 +0400

    templates begin - navbar pre-ready

--- ecomm_platform/store/models.py
+++ ecomm_platform/store/models.py
@@ -6,7 +6,7 @@ from django.core.validators import RegexValidator, MinValueValidator
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='static/img')
     default = models.BooleanField(default=False)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
@@ -33,6 +33,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
